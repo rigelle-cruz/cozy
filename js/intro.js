c1217b55d04e02a94d00e38347411588158638ac
Rigelle Cruz <rigellecruz@gmail.com>
2023-10-26 10:01:07 +1300

    create function for the intro screen transition

--- js/intro.js
+++ js/intro.js
@@ -1,10 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
   const introScreen = document.getElementById("intro-screen");
+  const room = document.querySelector(".room");
   const startButton = document.getElementById("start-button");
-  const gameRoom = document.querySelector(".room");
 
   startButton.addEventListener("click", function () {
     introScreen.style.display = "none";
-    gameRoom.style.display = "block";
+
+   
+    room.style.display = "block";
+    room.classList.add("room-fade-in");
   });
 });
