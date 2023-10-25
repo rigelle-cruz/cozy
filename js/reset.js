ecbaed549b714475a67ea9c494cf35754ba5b6e2
Rigelle Cruz <rigellecruz@gmail.com>
2023-10-26 12:51:41 +1300

    make clear room button functional

--- js/reset.js
+++ js/reset.js
@@ -1,9 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
   function fadeInRoom() {
-  const room = document.querySelector('.room');
-  room.classList.add('fade-in');
-}
+    const room = document.querySelector('.room');
+    room.classList.add('fade-in');
+  }
 
   fadeInRoom(); 
+
+  const reloadButton = document.getElementById('refresh');
+
+    reloadButton.addEventListener('click', function () {
+    location.reload();
+    });
+
 });
 
