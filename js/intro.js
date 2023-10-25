fc5bd999e6668711ed5bee1c8c355e75781bbdfe
Rigelle Cruz <rigellecruz@gmail.com>
2023-10-26 12:47:40 +1300

    fix intro and room pages

--- js/intro.js
+++ js/intro.js
@@ -1,13 +1,9 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const introScreen = document.getElementById("intro-screen");
-  const room = document.querySelector(".room");
-  const startButton = document.getElementById("start-button");
+function fadeAndRedirect() {
+  const button = document.getElementById('intro-screen');
+  button.style.opacity = 0;
+  button.style.transition = 'opacity 1s';
 
-  startButton.addEventListener("click", function () {
-    introScreen.style.display = "none";
-
-   
-    room.style.display = "block";
-    room.classList.add("room-fade-in");
-  });
-});
+  setTimeout(() => {
+    window.location.href = 'room.html';
+  }, 1000);
+}
---
+++ js/reset.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+  function fadeInRoom() {
+  const room = document.querySelector('.room');
+  room.classList.add('fade-in');
+}
+
+  fadeInRoom(); 
+});
+
